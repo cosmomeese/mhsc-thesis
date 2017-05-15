62d70a7791a19bc8faba3114b8d97e61516aac2d
Jonathan-F. Baril <jbaril@ehealthinnovation.org>
2017-05-15 14:41:26 -0400

    updated to ignore sheets for patients with no data (e.g. who dropped out of study)

--- analytics/userDefinedFcns/processSimonStepData.R
+++ analytics/userDefinedFcns/processSimonStepData.R
@@ -21,52 +21,57 @@ processSimonStepData <- function(importedData)
   #for every sheet apply
   for(sheetName in names(importedData))
   {
-  
     importedData.listItem <- importedData[[sheetName]]
-    ## Reformat Data for Use
-    #Make sure data frame names are compatible with R
-    names(importedData.listItem) <- make.names(names(importedData.listItem),
-                                               unique=FALSE)
     
-    #DateTime
-    inputFormat <- "%m/%d/%Y %H:%M:%S"
-    DateTime <- as.POSIXlt(importedData.listItem[[1]], 
-                           format = inputFormat) #access 1's column & change from 'character' (string) type to date 
-    processedData.listItem <- data.frame(DateTime) #remember df = data.frame(x) will be referred to as df$x
-    rm(DateTime) #remove DateTime since we only need it to initialize data.frame
-    
-    #Time
-    outputFormat <- "%H:%M:%S"
-    processedData.listItem['Time'] <- format(processedData.listItem$DateTime,
-                                    format=outputFormat) #access new DateTime column & change from POSIXlt format to time
-    
-    #Date  
-    outputFormat <- "%Y/%m/%d"
-    processedData.listItem['Date'] <- format(processedData.listItem$DateTime,
-                                    format=outputFormat) #access new DateTime column & change from POSIXlt format to time
-    
-    #Steps
-    processedData.listItem['Steps'] <- importedData.listItem$steps #make everything consistently uppercase
-    
-    ## Remove unneeded Data
-    processedData.listItem$DateTime <- NULL
-    
-    # Nest all other variables (Step & Time) for each Date
-    processedData.listItem <- tidyr::nest(data = dplyr::group_by(processedData.listItem,Date),
-                                          .key = "Intraday")
-    
-    processedData.listItem["StudyIdentifier"] <- as.factor(sheetName)
-    
-    # Combine data frame with our existing list of data frames
-    if(!exists("processedData")) # Create the data frame initially if it hasn't yet been created
+    if(!(is.null(importedData.listItem[["Date"]]) || 
+         is.null(importedData.listItem[["Steps"]]))) #i.e. if data exists
     {
-      processedData <- processedData.listItem
+      
+      ## Reformat Data for Use
+      #Make sure data frame names are compatible with R
+      names(importedData.listItem) <- make.names(names(importedData.listItem),
+                                                 unique=FALSE)
+      
+      #DateTime (and create new processedData object)
+      inputFormat <- "%m/%d/%Y %H:%M:%S"
+      DateTime <- as.POSIXlt(importedData.listItem[["Date"]], 
+                             format = inputFormat) #access Date's column & change from 'character' (string) type to date
+    
+      processedData.listItem <- data.frame(DateTime) #remember df = data.frame(x) will be referred to as df$x
+      rm(DateTime) #remove DateTime since we only need it to initialize data.frame
+      
+      #Time
+      outputFormat <- "%H:%M:%S"
+      processedData.listItem['Time'] <- format(processedData.listItem$DateTime,
+                                      format=outputFormat) #access new DateTime column & change from POSIXlt format to time
+      
+      #Date  
+      outputFormat <- "%Y/%m/%d"
+      processedData.listItem['Date'] <- format(processedData.listItem$DateTime,
+                                      format=outputFormat) #access new DateTime column & change from POSIXlt format to time
+      
+      #Steps
+      processedData.listItem['Steps'] <- importedData.listItem$steps #make everything consistently uppercase
+      
+      ## Remove unneeded Data
+      processedData.listItem$DateTime <- NULL
+      
+      # Nest all other variables (Step & Time) for each Date
+      processedData.listItem <- tidyr::nest(data = dplyr::group_by(processedData.listItem,Date),
+                                            .key = "Intraday")
+      
+      processedData.listItem["StudyIdentifier"] <- sheetName
+      
+      # Combine data frame with our existing list of data frames
+      if(!exists("processedData")) # Create the data frame initially if it hasn't yet been created
+      {
+        processedData <- processedData.listItem
+      }
+      else #Tack on the new data frame to the existing data frame
+      {
+        processedData <- rbind(processedData,processedData.listItem)
+      }
     }
-    else #Tack on the new data frame to the existing data frame
-    {
-      processedData <- rbind(processedData,processedData.listItem)
-    }
-
   }
   
   #tidy it up
