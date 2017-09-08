f0a0c98d5c5e5b37cb8203d65092b0034e19f151
Jonathan-F. Baril <jonathanf.baril+code@gmail.com>
2017-09-08 17:01:16 -0400

    R automated version updater

---
+++ analytics/versionUpdateR.R
@@ -0,0 +1,6 @@
+#Thank you installr package authors (Tal Galili et)
+
+# installing/loading the package:
+if(!require(installr)) { install.packages("installr"); require(installr)} #load / install+load installr
+
+updateR(F, T, T, F, T, F, T) # install, move, update.package, quit R.
